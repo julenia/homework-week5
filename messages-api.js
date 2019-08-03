51f6fbbdd414fb34f804a950c3c71d961b8db774
julenia <julia.jankowska@onet.eu>
2019-08-03 11:57:22 +0200

    number of requests checked as a middleware function and implemenation of it

--- messages-api.js
+++ messages-api.js
@@ -4,24 +4,23 @@ const app = express()
 const port = 3000
 const parserMiddleware = bodyParser.json()
 app.use(parserMiddleware)
-
 let numberOfRequests = 0;
 
+function tooManyRequests(req, res, next){
+  if(numberOfRequests>=5) return res.status(429).send()
+  next()
+}
 
-
-app.post('/message', (req, res, next) => {
+app.post('/message',tooManyRequests ,(req, res, next) => {
   const body = req.body
-  if(numberOfRequests > 5) res.status(429).send()
-  else {
   if (!body.message || body.message === "") {
-    numberOfRequests++
-    
+    numberOfRequests++ 
     return res.status(400).send()
   }
   else {
     numberOfRequests++
     return res.json(body)
   }}
-})
+)
 
 app.listen(port, ()=> console.log(`Listen to port: ${port}`))
